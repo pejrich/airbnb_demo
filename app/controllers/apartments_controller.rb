d1631ad892697261cdf452d656a83c2cdea716d3
Grace Yang <yaycake@gmail.com>
2017-11-20 18:58:20 +0800

    Completed stuff

--- app/controllers/apartments_controller.rb
+++ app/controllers/apartments_controller.rb
@@ -5,7 +5,20 @@ class ApartmentsController < ApplicationController
   API = "https://raw.githubusercontent.com/lewagon/flats-boilerplate/master/flats.json"
 
   def index
-    @data = JSON.parse(open(API).read)
+    @data = parse
   end
+
+  def show
+    @apt = params[:id]
+    @data = parse
+    @data.find {|apt| apt["name"] == @apt }
+  end
+
+  private
+
+  def parse
+    JSON.parse(open(API).read)
+  end
+
 end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-get '/apartments', to: "apartments#index"
+  get '/apartments', to: "apartments#index"
+
+  get '/apartments/:id', to:"apartments#show", as: :apartment
 
 end
