c0ba151aba104f51d8880a047f7e69d1b5248e94
Pitipon Pluemworasawat <pitipon@gmail.com>
2017-11-22 19:01:23 +0800

    create form for

--- yelp/app/models/review.rb
+++ yelp/app/models/review.rb
@@ -1,5 +1,6 @@
 class Review < ApplicationRecord
+  RATING = [0,1,2,3,4,5]
   validates :content, presence: true
-  validates :rating, inclusion: { in: [0,1,2,3,4,5], allow_nil: false }
+  validates :rating, inclusion: { in: RATING, allow_nil: false }
   belongs_to :restaurant
 end
